254e479c658d7805d5b584c6827a4c91df7a6951
Jay Sorg <jay.sorg@gmail.com>
2020-01-21 10:25:13 -0800

    tests: add random sleep time

--- tests/test_client/test_client.c
+++ tests/test_client/test_client.c
@@ -18,7 +18,9 @@ main(int argc, char** argv)
 {
     struct sockaddr_un s;
     int sck;
+    int ran;
     struct stream* out_s;
+    unsigned int stime;
 
     sck = socket(PF_LOCAL, SOCK_STREAM, 0);
     if (sck == -1)
@@ -26,6 +28,8 @@ main(int argc, char** argv)
         return 1;
     }
 
+    ran = open("/dev/urandom", O_RDONLY);
+    read(ran, &stime, 4);
     out_s = (struct stream*)calloc(1, sizeof(struct stream));
     out_s->data = (char*)malloc(1024);
     out_s->p = out_s->data;
@@ -33,11 +37,12 @@ main(int argc, char** argv)
     out_uint32_le(out_s, 9);
     memset(&s, 0, sizeof(struct sockaddr_un));
     s.sun_family = AF_UNIX;
-    strncpy(s.sun_path, "/tmp/wtv_hdhrd3138", sizeof(s.sun_path));
+    strncpy(s.sun_path, "/tmp/wtv_hdhrd0", sizeof(s.sun_path));
     s.sun_path[sizeof(s.sun_path) - 1] = 0;
     connect(sck, (struct sockaddr*)&s, sizeof(struct sockaddr_un));
     send(sck, out_s->data, 9, 0);
-    usleep(10 * 1024 * 1024);
+    printf("stime %d\n", stime % 10);
+    usleep((stime % 10) * 1024 * 1024);
     close(sck);
     return 0;
 }
