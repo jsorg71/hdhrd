4e4aac3027843e426fc48fd6e856ae8a03a1c79a
Jay Sorg <jay.sorg@gmail.com>
2020-03-08 00:07:47 -0800

    tests: use first uds found

--- tests/test_client/test_client.c
+++ tests/test_client/test_client.c
@@ -5,12 +5,47 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <time.h>
+#include <dirent.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 
 #include "arch.h"
 #include "parse.h"
+
+/*****************************************************************************/
+const char *
+get_filename(char* filename, int bytes)
+{
+    DIR * ldir;
+    struct dirent * entry;
+    int count;
+
+    count = 0;
+    ldir = opendir("/tmp");
+    if (ldir != NULL)
+    {
+        entry = readdir(ldir);
+        while (entry != NULL)
+        {
+            if (strncmp(entry->d_name, "wtv_", 3) == 0)
+            {
+                if (entry->d_type == DT_SOCK)
+                {
+                    snprintf(filename, bytes, "/tmp/%s", entry->d_name);
+                    count++;
+                }
+            }
+            entry = readdir(ldir);
+        }
+        closedir(ldir);
+    }
+    if (count == 1)
+    {
+        return filename;
+    }
+    return NULL;
+}
 
 /*****************************************************************************/
 int
@@ -21,13 +56,10 @@ main(int argc, char** argv)
     int ran;
     struct stream* out_s;
     unsigned int stime;
+    char filename[256];
+    const char* lfilename;
 
     sck = socket(PF_LOCAL, SOCK_STREAM, 0);
-    if (sck == -1)
-    {
-        return 1;
-    }
-
     ran = open("/dev/urandom", O_RDONLY);
     read(ran, &stime, 4);
     out_s = (struct stream*)calloc(1, sizeof(struct stream));
@@ -38,7 +70,9 @@ main(int argc, char** argv)
     out_uint8(out_s, 1);
     memset(&s, 0, sizeof(struct sockaddr_un));
     s.sun_family = AF_UNIX;
-    strncpy(s.sun_path, "/tmp/wtv_hdhrd_1447", sizeof(s.sun_path));
+    lfilename = get_filename(filename, 256);
+    printf("connecting filename %s\n", lfilename);
+    strncpy(s.sun_path, lfilename, sizeof(s.sun_path));
     s.sun_path[sizeof(s.sun_path) - 1] = 0;
     connect(sck, (struct sockaddr*)&s, sizeof(struct sockaddr_un));
     send(sck, out_s->data, 9, 0);
